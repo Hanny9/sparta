4e47d8ba485c9988b7673025ef2ccc88be8f563d
Hanny9 <52433350+Hanny9@users.noreply.github.com>
2020-03-15 19:06:10 +0900

    200315

---
+++ etc/기타.js
@@ -0,0 +1,9 @@
+function compare_ninety(num){
+    if (num > 90) {
+        alert('90보다 커요!')
+    } else if  (num == 90) {
+        alert('90과 같아요!')
+    } else (num < 90) {
+        alert('90보다 작아요!')
+    }
+}
